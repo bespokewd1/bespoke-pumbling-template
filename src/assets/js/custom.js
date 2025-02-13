1eed37af1e10b1c0a43b747e0963d7b5592daf95
Cyvior <bryanceazartabanas@gmail.com>
2025-02-13 22:03:11 +0800

    Homepage Banner

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -7,3 +7,17 @@ const faqItems = Array.from(document.querySelectorAll('.cs-faq-item'));
         }
         item.addEventListener('click', onClick)
         }
+        new BlazeSlider(el, {
+            all: {
+              enableAutoplay: true,
+              autoplayInterval: 2000,
+              transitionDuration: 300,
+              slidesToShow: 3,
+            },
+            '(max-width: 900px)': {
+              slidesToShow: 2,
+            },
+            '(max-width: 500px)': {
+              slidesToShow: 1,
+            },
+          })
